aa418dcf7d3a1db6e2cb1bbf186e1a22303f1644
JunCodera1 <minhtien0601200@gmail.com>
2025-06-08 10:25:25 +0700

    chore: add Nix environment setup and Prisma Docker helper script
    - Add .envrc and shell.nix for Nix development shell
    - Add flake.nix and flake.lock for Nix flakes support
    - Add docker-prisma.sh to manage Prisma in Docker
    - Update package.json and package-lock.json for dependency adjustments

---
+++ shell.nix
@@ -0,0 +1,15 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.nodejs_18
+    pkgs.openssl_1_1
+    pkgs.prisma-engines
+  ];
+
+  # Point Prisma to custom engines
+  PRISMA_QUERY_ENGINE_LIBRARY = "${pkgs.prisma-engines}/lib/libquery_engine.node";
+  PRISMA_QUERY_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/query-engine";
+  PRISMA_SCHEMA_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/schema-engine";
+  PRISMA_FMT_BINARY = "${pkgs.prisma-engines}/bin/prisma-fmt";
+}
