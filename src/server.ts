c393da46823cfdc688b66af558ddf3bc2a1bd584
JunCodera1 <minhtien0601200@gmail.com>
2025-06-11 22:39:13 +0700

    Added: Register route

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from "express";
+import authRoutes from "./routes/auth";
 const app = express();
 const port = 3000;
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-
+app.use("/auth", authRoutes);
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
 });
